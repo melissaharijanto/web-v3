c749f32921a9d16d883d7eb836048b5c7b06dbfc
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-18 00:54:05 +0800

    Add responsiveness to container type components

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -11,7 +11,7 @@ const HeroComponent = () => {
     <div
       className={`bg-light-background bg-cover w-full min-h-[80vh] flex flex-col justify-center rounded-3xl border-[1px] border-black shadow-solid
             0 25px 25px 0 2px rgb(1 1 1)]`}>
-      <div className="py-10 px-24 w-full flex items-center flex-col justify-center text-center">
+      <div className="py-10 px-12 md:px-16 lg:px-24 w-full flex items-center flex-col justify-center text-center">
         <div className="mb-20">
           <span className={`${fonts['text-small']}`}>{hero.header1}</span>
         </div>
@@ -20,14 +20,15 @@ const HeroComponent = () => {
             <span>{name}</span>
           ))}
         </div>
-        <div className={`flex flex-row mt-4`}>
+        <div
+          className={`flex flex-col items-center justify-center md:flex-row mt-4`}>
           <button
             className={`${fonts.subheading1} ${buttons.rounded}`}
             onClick={() => openPage(hero.buttonLink1)}>
             {hero.button1}
           </button>
           <button
-            className={`${fonts.subheading1} ${buttons.rounded} text-white bg-black ml-4`}
+            className={`${fonts.subheading1} ${buttons.rounded} text-white bg-black mt-2 md:mt-0 md:ml-2`}
             onClick={() => openPage(hero.buttonLink2)}>
             {hero.button2}
           </button>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,7 +4,7 @@ import { Project, projects } from '../constants/projects';
 
 const Projects: React.FC = (): ReactElement => {
   return (
-    <div className="w-100 grid grid-cols-3 p-24 gap-x-10 gap-y-10">
+    <div className="w-100 grid md:grid-cols-2 lg:grid-cols-3 p-12 md:p-16 lg:p-24 gap-x-10 gap-y-10">
       {projects.map((project: Project) => (
         <ProjectComponent project={project} />
       ))}
