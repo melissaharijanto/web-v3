bee8fc2cc27f9a293f1964a17d86be0220d2db4a
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-17 20:35:35 +0800

    Add styles and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'light-background': "url('./src/assets/images/light-background.jpg')"
+      }),
+      fontFamily: {
+        'archivo': ['"Archivo Black"', ...defaultTheme.fontFamily.sans],
+        'plus-jakarta-sans': ['"Plus Jakarta Sans"', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        'solid': '13px 13px 0 0 rgba(255,255,255,1.0), 13px 13px 0 2px rgba(0,0,0,1.0)',
+      }
+    },
   },
   plugins: [],
 }
