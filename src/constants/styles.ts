dc7031cdcce72e49f8c8aa270815af836bf2cc05
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-19 20:51:20 +0800

    Add styles

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,9 +1,9 @@
 export const fonts = {
-    "heading1" : "text-8xl font-archivo tracking-tighter",
-    "subheading1": "font-plus-jakarta-sans font-semibold text-xl tracking-widest",
-    "text-normal": "font-plus-jakarta-sans",
-    "text-small" : "font-plus-jakarta-sans tracking-[0.2em] font-medium",
-
+    "heading1" : "text-4xl md:text-6xl lg:text-8xl font-archivo tracking-tighter",
+    "subheading1": "font-plus-jakarta-sans font-semibold text-sm md:text-xl tracking-widest",
+    "text-normal": "font-plus-jakarta-sans text-sm md:text-md lg:text-lg",
+    "text-small" : "font-plus-jakarta-sans tracking-[0.2em] text-xs sm:text-sm md:text-md font-medium",
+    "text-link": "font-plus-jakarta-sans text-[#0096FF] text-sm md:text-md lg:text-lg font-bold hover:text-[#4169E1] hover:underline"
 }
 
 export const buttons = {
