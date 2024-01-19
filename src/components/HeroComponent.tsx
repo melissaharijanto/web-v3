396ea07138b25263e36890e47329e902536249c4
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-19 20:51:07 +0800

    Add mobile responsiveness for Hero containers

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -9,10 +9,10 @@ export const openPage = (link: string) => {
 const HeroComponent = () => {
   return (
     <div
-      className={`bg-light-background bg-cover w-full min-h-[80vh] flex flex-col justify-center rounded-3xl border-[1px] border-black shadow-solid
+      className={`bg-light-background bg-cover w-full min-h-[80vh] flex flex-col justify-center rounded-3xl border-[1px] border-black shadow-mobile lg:shadow-solid
             0 25px 25px 0 2px rgb(1 1 1)]`}>
       <div className="py-10 px-12 md:px-16 lg:px-24 w-full flex items-center flex-col justify-center text-center">
-        <div className="mb-20">
+        <div className="mb-10 md:mb-20">
           <span className={`${fonts['text-small']}`}>{hero.header1}</span>
         </div>
         <div className={`${fonts.heading1} flex flex-col text-center`}>
@@ -33,7 +33,7 @@ const HeroComponent = () => {
             {hero.button2}
           </button>
         </div>
-        <div className="mt-24">
+        <div className="mt-12 md:mt-24">
           <span className={`${fonts['text-small']}`}>{hero.footer1}</span>
           <br></br>
           <span className={`${fonts['text-small']}`}>{hero.footer2}</span>
