498783d8dbae1a6fd1bd439602262f1ccf62b08e
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-24 00:26:22 +0800

    Edit solid shadow to yellow

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,9 @@ module.exports = {
         'plus-jakarta-sans': ['"Plus Jakarta Sans"', ...defaultTheme.fontFamily.sans],
       },
       boxShadow: {
-        'solid': '13px 13px 0 0 rgba(255,255,255,1.0), 13px 13px 0 1px rgba(0,0,0,1.0)',
+        'solid': '13px 13px 0 0 rgba(255,240,200,1.0), 13px 13px 0 1px rgba(0,0,0,1.0)',
         'project': '7px 7px 0 0 rgba(255,240,200,1.0), 7px 7px 0 1px rgba(0,0,0,1.0)',
-        'mobile': '7px 7px 0 0 rgba(2255,255,255,1.0), 7px 7px 0 1px rgba(0,0,0,1.0)'
+        'mobile': '7px 7px 0 0 rgba(255,240,200,1.0), 7px 7px 0 1px rgba(0,0,0,1.0)'
       }
     },
   },
