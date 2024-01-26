a30e3153a61bbe8f55d165d882736f4b593592fa
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-27 01:15:18 +0800

    Base off navbar content in 1 file

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, useState } from 'react';
-import Logo from '../assets/images/black-logo.svg';
 import { fonts } from '../constants/styles';
+import navbar from '../constants/navbar';
 
 const NavigationBar: React.FC = (): ReactElement => {
   const linkStyle = `${fonts['text-small']} hover:font-extrabold transition-all ease-in cursor-pointer mx-4`;
@@ -20,20 +20,24 @@ const NavigationBar: React.FC = (): ReactElement => {
         onTop ? 'bg-transparent' : 'bg-white/60'
       }`}>
       <div className="flex justify-center items-center">
-        <img src={Logo} alt="logo" className="w-28 mx-6 top-0 my-4" />
+        <img
+          src={navbar['light-mode-logo']}
+          alt="logo"
+          className="w-28 mx-6 top-0 my-4"
+        />
       </div>
       <div className="flex justify-center items-center">
-        <a href="#home" className={linkStyle}>
-          HOME
+        <a href={`${navbar.link1}`} className={linkStyle}>
+          {navbar.link1.toUpperCase()}
         </a>
-        <a href="#about" className={linkStyle}>
-          ABOUT
+        <a href={`${navbar.link2}`} className={linkStyle}>
+          {navbar.link2.toUpperCase()}
         </a>
-        <a href="#experiences" className={linkStyle}>
-          EXPERIENCES
+        <a href={`${navbar.link3}`} className={linkStyle}>
+          {navbar.link3.toUpperCase()}
         </a>
-        <a href="#projects" className={`${linkStyle} mr-8`}>
-          PROJECTS
+        <a href={`${navbar.link4}`} className={`${linkStyle} mr-8`}>
+          {navbar.link4.toUpperCase()}
         </a>
       </div>
     </div>
---
+++ src/constants/navbar.ts
@@ -0,0 +1,13 @@
+import WhiteLogo from '../assets/images/white-logo.svg';
+import BlackLogo from '../assets/images/black-logo.svg';
+
+const navbar = {
+    "light-mode-logo": BlackLogo,
+    "dark-mode-logo": WhiteLogo,
+    "link1": "home",
+    "link2": "about",
+    "link3": "experiences",
+    "link4": "projects",
+}
+
+export default navbar;
