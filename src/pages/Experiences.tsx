2a79416417b9c92668d68164d7559516d9aa8304
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-27 02:45:33 +0800

    Add view my resume button

--- src/pages/Experiences.tsx
+++ src/pages/Experiences.tsx
@@ -1,16 +1,22 @@
 import React, { ReactElement } from 'react';
-import { fonts } from '../constants/styles';
+import { buttons, fonts } from '../constants/styles';
 import ExperienceComponent from '../components/ExperienceComponent';
 import { experiences } from '../constants/experiences';
 import navbar from '../constants/navbar';
+import { hero } from '../constants/hero';
 
 const Experiences: React.FC = (): ReactElement => {
   return (
     <div
       id={`${navbar.link3}`}
       className="flex flex-col items-center justify-center w-full p-6 md:p-16 lg:p-24 ">
-      <div className="mb-8 md:mb-14">
-        <span className={`${fonts.heading1}`}>EXPERIENCES</span>
+      <div className="flex flex-col justify-center items-center mb-8 md:mb-14">
+        <span className={`${fonts.heading1} mb-4`}>EXPERIENCES</span>
+        <a
+          href={hero.buttonLink1}
+          className={`${buttons.rounded} ${fonts.subheading1}`}>
+          VIEW MY RESUME
+        </a>
       </div>
       <div className="grid lg:grid-cols-2 w-full lg:w-[80%] gap-y-6 lg:gap-x-6">
         {experiences.map((exp) => (
