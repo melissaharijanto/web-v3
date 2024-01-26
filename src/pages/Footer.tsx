4fac6684bb4be78a2c91d617cbff6eb19dd9f0d2
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-27 03:03:50 +0800

    Edit footer

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -5,9 +5,6 @@ const Footer = () => {
   return (
     <div className="w-full py-12 px-6 flex flex-col justify-center items-center">
       <span className={`${fonts['text-normal']} text-center`}>
-        Prototyped in <strong>Figma</strong>.{' '}
-      </span>
-      <span className={`${fonts['text-normal']} text-center`}>
         Built with <strong>React with Typescript</strong> and{' '}
         <strong>Tailwind CSS</strong>.
       </span>
