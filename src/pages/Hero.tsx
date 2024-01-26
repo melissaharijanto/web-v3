415919a2d9fd1acc5dfd54561e61ff0ba3320804
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-27 01:10:20 +0800

    Adjust hero padding to accommodate navbar

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -3,7 +3,7 @@ import HeroComponent from '../components/HeroComponent';
 
 const Hero: React.FC = (): ReactElement => {
   return (
-    <div className="w-full p-6 md:p-16 lg:p-24">
+    <div className="w-full p-6 md:p-16 md:pt-24 lg:p-24 lg:pt-28">
       <HeroComponent />
     </div>
   );
