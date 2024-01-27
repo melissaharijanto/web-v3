1a72023c0c9c14ba6fb5bf80717864354ec3bdb4
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-27 18:24:10 +0800

    Put space between hero and navbar

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -6,7 +6,7 @@ const Hero: React.FC = (): ReactElement => {
   return (
     <div
       id={`${navbar.link1}`}
-      className="w-full p-6 md:p-16 md:pt-24 lg:p-24 lg:pt-28">
+      className="w-full p-6 pt-24 md:p-16 md:pt-24 lg:p-24 lg:pt-28">
       <HeroComponent />
     </div>
   );
