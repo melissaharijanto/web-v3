ee4f037c36c5f282a57178da25db998d706216f1
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-28 00:04:09 +0800

    Apply dark mode to app bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React, { ReactElement } from 'react';
 import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { RootState } from './redux/store';
-import { Mode, switchToDarkMode } from './redux/features/modeSlice';
+import { Mode } from './redux/features/modeSlice';
 import Hero from './pages/Hero';
 import Projects from './pages/Projects';
 import Experiences from './pages/Experiences';
 import About from './pages/About';
 import NavigationBar from './components/NavigationBar';
 import Footer from './pages/Footer';
+import colors, { ColorScheme, Colors } from './constants/colors';
 
 const App: React.FC = (): ReactElement => {
   const mode: Mode = useSelector((state: RootState) => state.config.mode);
-  const dispatch = useDispatch();
-
+  const bgColor: string = `${(colors[mode as keyof Colors] as ColorScheme).bg}`;
   return (
-    <div>
+    <div className={`bg-${bgColor}`}>
       <NavigationBar />
       <Hero />
       <About />
