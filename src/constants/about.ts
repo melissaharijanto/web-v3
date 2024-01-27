09704032f461891defda243c0808e8e05a5a0696
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-28 00:03:52 +0800

    Update about slightly

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -3,7 +3,7 @@ const about = {
     "buttonLink1": "https://github.com/melissaharijanto/",
     "button2": "Email",
     "buttonLink2": "mailto:melissaharijanto18@gmail.com",
-    "p1": "I am an undergraduate studying **Computer Science** in the **National University of Singapore**. I have experience in **Full Stack Development** as well as using **GenAI tools**, such as using **ChatGPT API** for development. I am especially passionate about **Frontend Development**, whether it is for web or mobile applications. I love applying what I have learnt to real life projects.",
+    "p1": "I am an undergraduate studying **Computer Science** in the **National University of Singapore**. I have experience in **Full Stack Development** as well as using **GenAI tools**, such as using **ChatGPT API** for development. I am especially passionate about **Frontend Development**, whether it is **for web or mobile applications**. I love applying what I have learnt to real life projects.",
     "p2": "When I have free time, I love designing colorful graphics; whether it be posters, websites or anything random!"
 }
 
