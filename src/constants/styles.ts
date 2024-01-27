b4f73f12549fa3aa93d4f114d3b94ad554379da6
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-27 18:51:36 +0800

    Modify text style slightly

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -2,7 +2,7 @@ export const fonts = {
     "heading1" : "text-4xl md:text-6xl lg:text-7xl font-archivo tracking-tighter",
     "subheading1": "font-plus-jakarta-sans font-semibold text-sm md:text-base tracking-widest",
     "text-normal": "font-plus-jakarta-sans text-sm md:text-base",
-    "text-small" : "font-plus-jakarta-sans tracking-[0.2em] text-sm sm:text-base font-medium",
+    "text-small" : "font-plus-jakarta-sans tracking-[0.2em] text-sm md:text-base font-medium",
     "text-link": "font-plus-jakarta-sans text-[#0096FF] text-sm md:text-base font-bold hover:text-[#4169E1] hover:underline"
 }
 
