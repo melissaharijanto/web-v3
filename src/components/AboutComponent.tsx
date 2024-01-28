12dbdc2ad7c3d4d8db6ef3f4a72c963d752ddeb8
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-28 20:59:24 +0800

    Apply dark mode to About page

--- src/components/AboutComponent.tsx
+++ src/components/AboutComponent.tsx
@@ -3,23 +3,46 @@ import { buttons, fonts } from '../constants/styles';
 import { openPage } from './HeroComponent';
 import about from '../constants/about';
 import FormattedParagraph from './FormattedParagraph';
+import { Mode } from '../redux/features/modeSlice';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import colors, { ColorScheme, Colors } from '../constants/colors';
 
 const AboutComponent: React.FC = (): ReactElement => {
+  const mode: Mode = useSelector((state: RootState) => state.config.mode);
+  const textColor: string = `${
+    (colors[mode as keyof Colors] as ColorScheme).text
+  }`;
+
   return (
-    <div className="bg-about bg-cover bg-center border-black border-[1px] p-8 md:p-12 rounded-3xl shadow-project w-full md:w-[70%] lg:w-[60%] gap-x-8 text-justify grid gap-y-4 lg:gap-y-0 lg:grid-cols-[3fr_7fr]">
+    <div
+      className={`${
+        mode === 'LIGHT'
+          ? 'bg-about shadow-project'
+          : 'bg-about-dark bg-center shadow-dark-mobile'
+      } bg-cover bg-center border-[1px] p-8 md:p-12 rounded-3xl w-full md:w-[70%] lg:w-[60%] gap-x-8 text-justify grid gap-y-4 lg:gap-y-0 lg:grid-cols-[3fr_7fr]`}
+      style={{ borderColor: textColor }}>
       <div className="flex flex-col w-full order-last lg:order-first">
         <button
           onClick={() => openPage(about.buttonLink1)}
-          className={`${buttons.rounded} ${fonts.subheading1} w-full mb-2`}>
+          className={`${
+            mode === 'LIGHT'
+              ? buttons['rounded-light']
+              : buttons['rounded-dark']
+          } ${fonts.subheading1} w-full mb-2`}>
           {about.button1.toUpperCase()}
         </button>
         <button
           onClick={() => openPage(about.buttonLink2)}
-          className={`${buttons.rounded} ${fonts.subheading1} w-full`}>
+          className={`${
+            mode === 'LIGHT'
+              ? buttons['rounded-light']
+              : buttons['rounded-dark']
+          } ${fonts.subheading1} w-full`}>
           {about.button2.toUpperCase()}
         </button>
       </div>
-      <div>
+      <div style={{ color: textColor }}>
         <FormattedParagraph text={about.p1} />
         <br />
         <br />
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,14 +2,24 @@ import React, { ReactElement } from 'react';
 import { fonts } from '../constants/styles';
 import navbar from '../constants/navbar';
 import AboutComponent from '../components/AboutComponent';
+import { Mode } from '../redux/features/modeSlice';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import colors, { ColorScheme, Colors } from '../constants/colors';
 
 const About: React.FC = (): ReactElement => {
+  const mode: Mode = useSelector((state: RootState) => state.config.mode);
+  const textColor: string = `${
+    (colors[mode as keyof Colors] as ColorScheme).text
+  }`;
   return (
     <div
       id={`${navbar.link2}`}
       className="min-h-[90vh] flex flex-col items-center justify-center w-full p-8 md:p-16 lg:p-24">
       <div className="mb-8 md:mb-14">
-        <span className={`${fonts.heading1}`}>ABOUT</span>
+        <span className={`${fonts.heading1}`} style={{ color: textColor }}>
+          ABOUT
+        </span>
       </div>
       <AboutComponent />
     </div>
