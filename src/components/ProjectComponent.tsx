e338dd4bd86c17fd46e7abfd986e41aa6d7c3a39
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-01-28 21:00:36 +0800

    Apply dark mode to projects page

--- src/components/ProjectComponent.tsx
+++ src/components/ProjectComponent.tsx
@@ -1,32 +1,55 @@
 import React, { ReactElement } from 'react';
 import { Project } from '../constants/projects';
 import { fonts } from '../constants/styles';
+import { Mode } from '../redux/features/modeSlice';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import colors, { ColorScheme, Colors } from '../constants/colors';
 
 interface ProjectComponentProps {
   project: Project;
 }
 const ProjectComponent = ({ project }: ProjectComponentProps): ReactElement => {
+  const mode: Mode = useSelector((state: RootState) => state.config.mode);
+  const textColor: string = `${
+    (colors[mode as keyof Colors] as ColorScheme).text
+  }`;
+  const bgColor: string = `${(colors[mode as keyof Colors] as ColorScheme).bg}`;
+
   return (
-    <div className="flex flex-col rounded-2xl shadow-project border-[1px] border-black">
+    <div
+      className={`flex flex-col rounded-2xl ${
+        mode === 'LIGHT' ? 'shadow-project' : 'shadow-dark-mobile'
+      } border-[1px]`}
+      style={{ borderColor: textColor }}>
       <div className="w-100 px-6 py-4 flex items-center">
         <img
           src={project.icon}
-          className="w-8 h-8 rounded-2xl mr-2 border-[1px] border-black"
+          className={`w-8 h-8 rounded-2xl mr-2 border-[1px] border-${textColor}`}
+          style={{ color: textColor }}
           alt="Icon"
         />
-        <span className={`${fonts['text-normal']} font-bold`}>
+        <span
+          className={`${fonts['text-normal']} font-bold text-${textColor}`}
+          style={{ color: textColor }}>
           {project.username}
         </span>
       </div>
-      <div className="w-100 flex border-y-[1px] border-black">
+      <div
+        className={`w-100 flex border-y-[1px] border-${textColor}`}
+        style={{ color: textColor }}>
         <img src={project.image} alt={project.name} />
       </div>
       <div className="w-100 p-6 flex flex-col">
         <span className="text-justify">
-          <span className={`${fonts['text-normal']} font-bold`}>
+          <span
+            className={`${fonts['text-normal']} font-bold text-${textColor}`}
+            style={{ color: textColor }}>
             {project.name}
           </span>
-          <span className={`${fonts['text-normal']}`}>
+          <span
+            className={`${fonts['text-normal']} text-${textColor}`}
+            style={{ color: textColor }}>
             &nbsp;&mdash;&nbsp;{project.description}
           </span>
         </span>
@@ -40,7 +63,9 @@ const ProjectComponent = ({ project }: ProjectComponentProps): ReactElement => {
                 className={`${fonts['text-link']}`}>
                 {link[0]}
               </a>
-              {index < project.links.length - 1 && <span>&ensp;|&ensp;</span>}
+              {index < project.links.length - 1 && (
+                <span style={{ color: textColor }}>&ensp;|&ensp;</span>
+              )}
             </>
           ))}
         </span>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,14 +3,24 @@ import ProjectComponent from '../components/ProjectComponent';
 import { Project, projects } from '../constants/projects';
 import { fonts } from '../constants/styles';
 import navbar from '../constants/navbar';
+import { Mode } from '../redux/features/modeSlice';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import colors, { ColorScheme, Colors } from '../constants/colors';
 
 const Projects: React.FC = (): ReactElement => {
+  const mode: Mode = useSelector((state: RootState) => state.config.mode);
+  const textColor: string = `${
+    (colors[mode as keyof Colors] as ColorScheme).text
+  }`;
   return (
     <div
       id={`${navbar.link4}`}
       className="flex flex-col p-6 md:p-16 lg:p-24 w-100 justify-center items-center">
       <div className="mb-8 md:mb-14">
-        <span className={`${fonts.heading1}`}>PROJECTS</span>
+        <span className={`${fonts.heading1}`} style={{ color: textColor }}>
+          PROJECTS
+        </span>
       </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-6 md:gap-x-10 md:gap-y-10 lg:gap-x-14 lg:gap-y-14">
         {projects.map((project: Project) => (
